07c126f88ac5bd718c71f03aff0e557cbda69b1c
Lucas <lukass.orth@gmail.com>
2023-11-06 13:14:59 -0300

    deploy

--- android/app/src/main/kotlin/com/example/prova_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.prova_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/routes/routes.dart
+++
@@ -1,7 +0,0 @@
-import 'package:prova_flutter/views/home.dart';
-import 'package:prova_flutter/views/login.dart';
-
-class Routes {
-  static const home = HomeView();
-  static const login = LoginView();
-}
